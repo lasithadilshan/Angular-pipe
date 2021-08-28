d85c0ad45786ed4e0e691748adac54bfd5e33d38
lasithadilshan <dilshantilakaratne29@gmail.com>
2021-08-28 14:11:45 +0530

    2 files modified, 1 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,11 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 import { MyCompComponent } from './my-comp/my-comp.component';
+import { FirstofPipe } from './firstof.pipe';
 
 @NgModule({
   imports: [BrowserModule, FormsModule],
-  declarations: [AppComponent, HelloComponent, MyCompComponent],
+  declarations: [AppComponent, HelloComponent, MyCompComponent, FirstofPipe],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
